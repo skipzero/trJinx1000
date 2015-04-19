5b435951410dcd6514b20f3e46aad8b779396a50
skipZero <skip@zerosquadron.com>
2015-04-19 16:34:44 -0700

    added ext

--- checker.lua
+++ checker.lua
@@ -3,13 +3,12 @@ local tr = turtle
 function preStart()
   local fuelSlot = tr.select(1)
   local currFuel = tr.getFuelLevel()
-  local fuelLimit = tr.getFuelLimit()
   print(fuelSlot, currFuel)
 
-  if currFuel < fuelLimit then
+  if currFuel < 5 then
     tr.refuel()
+    print('turtle refueled, boss!')
   end
-  print('turtle refueled, boss! current level is ', currFuel, ' out of ', fuelLimit )
 end
 
 preStart()
